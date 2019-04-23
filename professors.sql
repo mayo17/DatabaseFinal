de1e1d7cb46e78c8d095f023842ca703532b21e3
mayo17 <45338543+mayo17@users.noreply.github.com>
2019-04-23 16:37:48 -0400

    fixed professors and students.sql

--- professors.sql
+++ professors.sql
@@ -1,23 +1,27 @@
+DROP TABLE IF EXISTS professors;
 CREATE TABLE professors (
-    pid INT AUTO_INCREMENT,
-    fname VARCHAR(20),
-    lname VARCHAR(20),
-    dob DATETIME,
+    pid INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    fname VARCHAR(20) NOT NULL,
+    lname VARCHAR(20) NOT NULL,
+    dob DATE NOT NULL,
     state VARCHAR(20),
-    zip INT,
+    zip INTEGER,
     salary FLOAT(200,2),
-    user VARCHAR(20),
-    pass VARCHAR(20),
-    PRIMARY KEY (pid)
+    user VARCHAR(20) NOT NULL,
+    pass VARCHAR(20) NOT NULL
 );
-INSERT INTO professors VALUES
-    (1001,'Harry','Johns','1987-09-21 00:00:00','California',90210,56000,'hjohns','pnut6'),
-    (1002,'Lloyd','Smith','1974-03-27 00:00:00','Mississippi',38601,48000,'lsmith','13ford'),
-    (1003,'Herbert','Jones','1962-08-11 00:00:00','Alabama',35487,39000,'hjones','river3'),
-    (1004,'William','Mays','1956-05-03 00:00:00','Florida',33635,52000,'wmays','lighthouse'),
-    (1005,'Marsha','Ford','1979-07-15 00:00:00','Georgia',30322,59000,'mford','orange7'),
-    (1006,'Phillip','Johnson','1963-02-05 00:00:00','North Carolina',27708,61000,'pjohnson','htj1985'),
-    (1007,'Sandra','Wallace','1985-04-24 00:00:00','Montana',59717,54000,'swallace','batgirl2'),
-    (1008,'Penny','Fink','1983-10-22 00:00:00','Illinois',60637,53000,'pfink','newton'),
-    (1009,'Floyd','Clark','1959-11-20 00:00:00','Florida',33146,49000,'fclark','joule54'),
-    (1010,'Ron','Adams','1980-06-12 00:00:00','Georgia',31207,50000,'radams','cougar');
+
+ALTER TABLE professors AUTO_INCREMENT=5001;
+
+INSERT INTO professors (fname, lname, dob, state, zip, salary, user, pass)
+VALUES
+    ('Harry','Johns','1987-09-21','California',90210,56000,'hjohns','pnut6'),
+    ('Lloyd','Smith','1974-03-27 ','Mississippi',38601,48000,'lsmith','13ford'),
+    ('Herbert','Jones','1962-08-11','Alabama',35487,39000,'hjones','river3'),
+    ('William','Mays','1956-05-03','Florida',33635,52000,'wmays','lighthouse'),
+    ('Marsha','Ford','1979-07-15','Georgia',30322,59000,'mford','orange7'),
+    ('Phillip','Johnson','1963-02-05','North Carolina',27708,61000,'pjohnson','htj1985'),
+    ('Sandra','Wallace','1985-04-24','Montana',59717,54000,'swallace','batgirl2'),
+    ('Penny','Fink','1983-10-22','Illinois',60637,53000,'pfink','newton'),
+    ('Floyd','Clark','1959-11-20','Florida',33146,49000,'fclark','joule54'),
+    ('Ron','Adams','1980-06-12','Georgia',31207,50000,'radams','cougar');
--- students.sql
+++ students.sql
@@ -1,22 +1,24 @@
+DROP TABLE IF EXISTS students;
 CREATE TABLE students (
-    sid INT AUTO_INCREMENT,
+    sid INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     fname VARCHAR(20) NOT NULL,
     lname VARCHAR(20) NOT NULL,
-    dob DATETIME NOT NULL,
+    dob DATE NOT NULL,
     state VARCHAR(20),
-    zip INT,
+    zip INTEGER,
     user VARCHAR(20) NOT NULL,
-    pass VARCHAR(20) NOT NULL,
-    PRIMARY KEY (sid)
+    pass VARCHAR(20) NOT NULL
 );
-INSERT INTO students VALUES
-    (1,'Jonathan','Ross','1990-03-01 00:00:00','Florida',33635,'bross','apple1'),
-    (2,'Daniel','Williams','1986-09-21 00:00:00','New York',10014,'dwilliams','4321'),
-    (3,'Henry','Newman','1993-06-12 00:00:00','Georgia',31207,'hnewman','pickle'),
-    (4,'Sally','Smith','1992-07-02 00:00:00','Florida',32306,'ssmith','pear2'),
-    (5,'Jane','Jones','1995-05-28 00:00:00','Nebraska',68588,'jjones','bluesky'),
-    (6,'William','Roberts','1987-03-05 00:00:00','Florida',32611,'wroberts','grass15'),
-    (7,'Mary','Wallace','1990-02-17 00:00:00','Florida',33620,'mwallace','99redball'),
-    (8,'Robert','Jones','1994-04-11 00:00:00','Tennessee',37996,'rjones','bball94'),
-    (9,'Paul','Clark','1996-11-25 00:00:00','Georgia',30332,'pclark','hawks'),
-    (10,'Jennifer','Adams','1991-10-04 00:00:00','Georgia',31207,'jadams','gobulls');
+INSERT INTO students (fname, lname, dob, state, zip, user, pass)
+VALUES
+    ('Jonathan','Ross','1990-03-01','Florida',33635,'bross','apple1'),
+    ('Daniel','Williams','1986-09-21','New York',10014,'dwilliams','4321'),
+    ('Henry','Newman','1993-06-12','Georgia',31207,'hnewman','pickle'),
+    ('Sally','Smith','1992-07-02','Florida',32306,'ssmith','pear2'),
+    ('Jane','Jones','1995-05-28','Nebraska',68588,'jjones','bluesky'),
+    ('William','Roberts','1987-03-05','Florida',32611,'wroberts','grass15'),
+    ('Mary','Wallace','1990-02-17','Florida',33620,'mwallace','99redball'),
+    ('Robert','Jones','1994-04-11','Tennessee',37996,'rjones','bball94'),
+    ('Paul','Clark','1996-11-25','Georgia',30332,'pclark','hawks'),
+    ('Jennifer','Adams','1991-10-04','Georgia',31207,'jadams','gobulls');
+
