7d485bd29f1a51b16e4bee27e40af5b21bc53886
mayo17 <45338543+mayo17@users.noreply.github.com>
2019-04-23 19:10:36 -0400

    added shell files for account and search pages

---
+++ search.js
@@ -0,0 +1,17 @@
+require('./renderer.js')
+
+document.getElementById("generate").addEventListener("click", () => {
+
+    // Perform a query to check how many people are logged in. 
+    $query = 'SELECT * FROM logging'
+    connect.query($query, function (err, rows, fields) {
+        if (err) {
+            console.log("An error ocurred while performing the query.")
+            console.log(err)
+            return
+        }
+        console.log("Query succesfully executed", rows)
+        document.getElementById("status").innerHTML = rows.length + " user(s) logged in"
+    })
+
+}, false)
